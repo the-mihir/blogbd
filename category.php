65ebaf4402e0d004a7f5fbca2c47677d590db430
Mihir Das <mihirkantho@gmail.com>
2024-09-08 16:14:30 +0600

    adding html templete as php website

---
+++ category.php
@@ -0,0 +1,193 @@
+<?php include_once 'inc/header.php'; ?>
+
+
+    <section class="site-section pt-5">
+      <div class="container">
+        <div class="row mb-4">
+          <div class="col-md-6">
+            <h2 class="mb-4">Category: Food</h2>
+          </div>
+        </div>
+        <div class="row blog-entries">
+          <div class="col-md-12 col-lg-8 main-content">
+            <div class="row mb-5 mt-5">
+
+              <div class="col-md-12">
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_10.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_11.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_12.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_9.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_8.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_7.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_6.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_5.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+                <div class="post-entry-horzontal">
+                  <a href="blog-single.php">
+                    <div class="image element-animate" data-animate-effect="fadeIn" style="background-image: url(images/img_4.jpg);"></div>
+                    <span class="text">
+                      <div class="post-meta">
+                        <span class="author mr-2"><img src="images/person_1.jpg" alt="Colorlib"> Colorlib</span>&bullet;
+                        <span class="mr-2">March 15, 2018 </span> &bullet;
+                        <span class="mr-2">Food</span> &bullet;
+                        <span class="ml-2"><span class="fa fa-comments"></span> 3</span>
+                      </div>
+                      <h2>There’s a Cool New Way for Men to Wear Socks and Sandals</h2>
+                    </span>
+                  </a>
+                </div>
+                <!-- END post -->
+
+              </div>
+            </div>
+
+            <div class="row mt-5">
+              <div class="col-md-12 text-center">
+                <nav aria-label="Page navigation" class="text-center">
+                  <ul class="pagination">
+                    <li class="page-item  active"><a class="page-link" href="#">&lt;</a></li>
+                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                    <li class="page-item"><a class="page-link" href="#">4</a></li>
+                    <li class="page-item"><a class="page-link" href="#">5</a></li>
+                    <li class="page-item"><a class="page-link" href="#">&gt;</a></li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+
+            
+
+          </div>
+
+          <!-- END main-content -->
+            <?php include_once 'inc/sidebar.php'; ?>
+          <!-- END sidebar -->
+
+        </div>
+      </div>
+    </section>
+  
+<?php include_once 'inc/footer.php'; ?>
