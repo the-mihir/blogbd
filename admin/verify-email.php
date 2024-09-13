cd11da0b98a13f39e753b43b889a2adfe7bff7f0
Mihir Das <mihirkantho@gmail.com>
2024-09-13 22:46:37 +0600

    Verifiy email authontication

--- admin/verify-email.php
+++ admin/verify-email.php
@@ -1,2 +1,19 @@
 <?php
 
+require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . '/../config/bootstrap.php';   
+
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mime\Email;
+
+$transport = Transport::fromDsn($_ENV['MAILER_DSN']);
+$mailer = new Mailer($transport);
+
+$email = (new Email())
+    ->from($_ENV['MAILER_FROM'])
+    ->to($_ENV['MAILER_TO'])
+    ->subject('Verify your email')
+    ->text('Thanks for signing up!');
+$mailer->send($email);  
+
